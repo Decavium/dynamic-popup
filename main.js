c56c48c821bed3d18a6563d3a6a5e20222193a38
Leo Williams <leokabirwilliams@gmail.com>
2023-12-13 15:30:04 +1100

    Added new intro and outro animations and finished functionality, changed button. Will now focus on styling.

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ var popupWrapper = document.querySelector(".popup-wrapper");
 var _loop_1 = function (i) {
     popupArray[i].querySelectorAll(".popup-button").forEach(function (button) { return button.addEventListener("pointerdown", function () { showNextPopup(popupArray[i], popupArray[i + 1]); }); });
 };
-// Skeleton if info not found
-// Array of input info - generate new cards for each array
-// const node = document.getElementById("myList2").lastChild;
-// const clone = node.cloneNode(true);
 for (var i = 0; i < popupArray.length - 1; i++) {
     _loop_1(i);
 }
@@ -14,16 +10,8 @@ popupArray[popupArray.length - 1].querySelectorAll(".popup-button").forEach(func
 function showNextPopup(element, nextElement) {
     element.classList.remove("active");
     nextElement.classList.add("active");
-    console.log("Element removed.");
 }
 function removePopupWrapper(wrapper) {
-    console.log("Wrapper removed.");
-    // wrapper.classList.add("hidden");
-    // wrapper.style.setProperty('animation', 'fadeOutButton 0.6s');
-    // wrapper.addEventListener("animationend",
-    //     () => {
-    //         // set to 0 as it reverts to its original opactiy before the animation happened
-    //         wrapper.style.opacity = '0';
-    //     }
-    // );
+    wrapper.classList.add("hidden");
+    setTimeout(function () { wrapper.remove(); }, 1000);
 }
--- main.ts
+++ main.ts
@@ -1,35 +1,20 @@
 const popupArray: HTMLElement[] = Array.from(document.getElementsByClassName('popup') as HTMLCollectionOf<HTMLElement>);
 const popupWrapper: HTMLElement = document.querySelector(".popup-wrapper") as HTMLElement;
 
-// Skeleton if info not found
-
-// Array of input info - generate new cards for each array
-// const node = document.getElementById("myList2").lastChild;
-// const clone = node.cloneNode(true);
-
 for (let i: number = 0; i < popupArray.length - 1; i++) {
     popupArray[i].querySelectorAll(".popup-button").forEach((button) => button.addEventListener("pointerdown", () => { showNextPopup(popupArray[i], popupArray[i + 1]) }))
 }
+// the last button calls the removePopupWrapper() function
 popupArray[popupArray.length - 1].querySelectorAll(".popup-button").forEach((button) => button.addEventListener("pointerdown", () => { removePopupWrapper(popupWrapper) }));
 
-function showNextPopup(element: Element, nextElement: Element) {
+function showNextPopup(element: HTMLElement, nextElement: HTMLElement) {
     element.classList.remove("active");
     nextElement.classList.add("active");
-
-    console.log("Element removed.");
 }
 
 function removePopupWrapper(wrapper: HTMLElement) {
-
-    console.log("Wrapper removed.");
-
-    // wrapper.classList.add("hidden");
-    // wrapper.style.setProperty('animation', 'fadeOutButton 0.6s');
-    // wrapper.addEventListener("animationend",
-    //     () => {
-    //         // set to 0 as it reverts to its original opactiy before the animation happened
-    //         wrapper.style.opacity = '0';
-    //     }
-    // );
+    wrapper.classList.add("hidden");
+    // removes the wrapper once the .3s transition finishes
+    setTimeout(() => { wrapper.remove() }, 300);
 }
 
